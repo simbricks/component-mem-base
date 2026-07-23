89cfc7bbd6f62aff207260abeb815b4cc852033f
Marvin Meiers <marvin@simbricks.io>
2026-07-23 12:37:21 +0200

    conda-recipes: do not set simbricks lib dir in build.sh

--- conda-recipes/simbricks-mem-base-sim-bin/build.sh
+++ conda-recipes/simbricks-mem-base-sim-bin/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -eo pipefail
 
-make sims-install \
-    PREFIX="${PREFIX}" \
-    SIMBRICKS_INC_DIR="${PREFIX}/include" \
-    SIMBRICKS_LIB_DIR="${PREFIX}/lib/simbricks"
+make sims-install PREFIX="${PREFIX}"
